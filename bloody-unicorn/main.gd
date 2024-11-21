a6ddbb77aefd03878f3954e37f62de92ac86004f
kbg2201 <111537927+kbg2201@users.noreply.github.com>
2024-11-20 20:55:36 -0700

    Trying to implement map

--- bloody-unicorn/main.gd
+++ bloody-unicorn/main.gd
@@ -4,10 +4,10 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#set background size (this is bad and sucks)
-	$TextureRect.size = Vector2i(
-		get_window().get_size_with_decorations().x,
-		get_window().get_size_with_decorations().y
-	)
+	#$TextureRect.size = Vector2i(
+		#get_window().get_size_with_decorations().x,
+		#get_window().get_size_with_decorations().y
+	#)
 	pass # Replace with function body.
 
 
